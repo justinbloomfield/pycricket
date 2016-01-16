6fef859771e2ab4cc36b3a226d66dea5f94c3dd6
Justin Bloomfield <jbloomfield@live.com>
2016-01-16 23:04:57 +0000

    Add pycricket.py

---
+++ pycricket.py
@@ -0,0 +1,31 @@
+#Initialise global variables
+global t1_runs
+global t2_runs
+global t1_name
+global t2_name
+global t1_outs
+global t2_outs
+global overs
+global ball
+global innings
+global bowl_type
+
+
+# FUNCTIONS
+
+def game(); #runs the functions together
+	
+	while innings = 1; #first half of the game
+		while overs >= 0;
+
+	
+
+def bowl();
+	global overs
+	global bowl_type
+	bowl_type = str(input("Do you wish to bowl pace, spin or off-spin? [p/s/o]: "))
+	if bowl_type = "p"
+		speed = randint(90,131)
+		print("Bowled at " + speed + "km/h.")
+	if bowl_type = "s"
+		speed = 
